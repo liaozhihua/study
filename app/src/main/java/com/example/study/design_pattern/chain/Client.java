240158d3bd1d9682a4db48afe5a2db7b522a5218
liaozh <liaozh@akulaku.com>
2022-04-08 14:06:02 +0800

    设计模式-行为型模式 责任链模式

--- app/src/main/java/com/example/study/design_pattern/chain/Client.java
+++ app/src/main/java/com/example/study/design_pattern/chain/Client.java
@@ -1,10 +1,5 @@
 package com.example.study.design_pattern.chain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import kotlin.collections.ArrayDeque;
-
 /**
  * <pre>  责任链模式
  * 如果这种结构用在编程领域，则每个节点可以看做一个对象，每个对象有不同的处理逻辑，将一个请求从链的首端发出，沿着链的路径依次传递每个节点对象，直到有对象处理这个请求为止，我们将这样一种模式称为责任链模式。
